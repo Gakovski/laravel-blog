5d5b94a37b5a0f9284a06e879a76efd2bccc1a61
Gakovski <david_gakovski@outlook.com>
2023-01-08 23:34:04 +0100

    Validating with custom request classes

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -7,13 +7,15 @@
     <div class="post-content">
         <h1>{{ $post->title }}</h1>
         <p>{{ $post->description }}</p>
-        <div class="buttons">
-            <div class="action_btn">
+        <div class="inline-flex-parent">
+            <div>
+                <button class="edit" type="submit"><a href="{{ route('posts.edit', [$post]) }}">Edit post</a></button>
+            </div>
+            <div>
                 <form method="POST" action="{{ route('posts.destroy', [$post]) }}">
-                    <button class="action_btn edit" type="submit"><a href="{{ route('posts.edit', [$post]) }}">Edit post</a></button>
                     @csrf
                     @method('DELETE')
-                    <button class="action_btn delete" type="submit">Delete post</button>
+                    <button class="delete" type="submit">Delete post</button>
                 </form>
             </div>
         </div>
