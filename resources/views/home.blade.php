fd8d8f63361375472757d721af5cc0b98f3c3ea5
Gakovski <david_gakovski@outlook.com>
2023-01-09 01:06:11 +0100

    Implementing Authentication

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,15 +4,16 @@
 
 @section('content')
 
+<div class="grid-parent">
 @forelse($posts as $post)
-<div class="post-item">
-    <div class="post-content">
-        <h2><a href="{{ route('posts.show', [$post]) }}">{{ $post->title }}</a></h2>
-        <p>{{ $post->description }}</p>
+    <div class="post-item">
+        <div class="post-content">
+            <h2><a href="{{ route('posts.show', [$post]) }}">{{ $post->title }}</a></h2>
+            <p>{{ $post->description }}</p>
+        </div>
     </div>
-</div>
 @empty
 <h2>There are no posts yes... :\</h2>
 @endforelse
-
+</div>
 @endsection
